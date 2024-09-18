f95fe349d9279e51bf2bc899916427fc8a94eda0
Programming Hero <programming.hero1@gmail.com>
2024-09-18 15:23:11 -0500

    practiced add money function

--- js/addMoney.js
+++ js/addMoney.js
@@ -3,9 +3,22 @@ document.getElementById('btn-add-money')
     event.preventDefault();
 
     const addMoney = document.getElementById('input-add-money').value;
-
+    const addMoneyNumber = parseFloat(addMoney);
+    
     const pinNumber = document.getElementById('input-pin-number').value;
 
     console.log(addMoney, pinNumber);
+    // wrong way to verify pin
+    if(pinNumber === '1234'){
+        const balance = document.getElementById('account-balance').innerText;
+        const balanceNumber = parseFloat(balance);
+
+        const newBalance = balanceNumber + addMoneyNumber;
+
+        document.getElementById('account-balance').innerText = newBalance;
+    }
+    else{
+        alert('Failed to add money.')
+    }
 
 })
